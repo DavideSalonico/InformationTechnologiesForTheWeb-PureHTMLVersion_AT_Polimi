cbd000a0481b5ccefd78e279ce8dddeac4be3fd6
DavideSalonico <davide.salonico@mail.polimi.it>
2023-05-17 17:08:47 +0200

    added filter

---
+++ src/main/java/filters/LoginChecker.java
@@ -0,0 +1,53 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/*")
+public class LoginChecker implements Filter{
+
+	public LoginChecker() {
+		super();
+	}
+
+	@Override
+	public void destroy() {
+		Filter.super.destroy();
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		Filter.super.init(filterConfig);
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		System.out.print("Login checker filter executing ...\n");
+
+		// java.lang.String login path = "/index.html";
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String loginpath = req.getServletContext().getContextPath() + "/index.html";
+
+		HttpSession s = req.getSession();
+		if (s.isNew() || s.getAttribute("user") == null) {
+			res.sendRedirect(loginpath);
+			return;
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+}
